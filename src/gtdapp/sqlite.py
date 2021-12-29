8d33a383ab17e7ef3c0388b1b173dafa45bdf1a6
Akira Hayashi <smxshxishxad@me.com>
2021-12-30 01:11:47 +0900

    :innocent: Init database at start

--- src/gtdapp/sqlite.py
+++ src/gtdapp/sqlite.py
@@ -3,7 +3,10 @@ import sqlite3
 conn = sqlite3.connect("todo.db")
 print("Opened database successfully")
 
-conn.execute("""CREATE TABLE IF NOT EXISTS ITEM
+
+conn.execute("DROP TABLE IF EXISTS ITEM;")
+
+conn.execute("""CREATE TABLE ITEM
          (ID INT PRIMARY KEY     NOT NULL,
          TITLE           TEXT    NOT NULL,
          DESCRIPTION     CHAR(200));""")
