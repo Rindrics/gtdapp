5c2fc7981e2c858ad3cd1294bbb8073f173a9c8f
Akira Hayashi <smxshxishxad@me.com>
2021-12-30 00:58:36 +0900

    :clapper: Insert inital data

--- src/gtdapp/sqlite.py
+++ src/gtdapp/sqlite.py
@@ -3,10 +3,21 @@ import sqlite3
 conn = sqlite3.connect("todo.db")
 print("Opened database successfully")
 
-conn.execute("""CREATE TABLE ITEM
+conn.execute("""CREATE TABLE IF NOT EXISTS ITEM
          (ID INT PRIMARY KEY     NOT NULL,
          TITLE           TEXT    NOT NULL,
          DESCRIPTION     CHAR(200));""")
 print("Table created successfully")
+
+
+conn.execute("INSERT INTO ITEM (ID,TITLE,DESCRIPTION) \
+      VALUES (1, 'hoge', 'hogehoge')");
+conn.execute("INSERT INTO ITEM (ID,TITLE,DESCRIPTION) \
+      VALUES (2, 'fuga', 'fugafuga')");
+conn.execute("INSERT INTO ITEM (ID,TITLE,DESCRIPTION) \
+      VALUES (3, 'piyo', 'piyopiyo')");
+
+conn.commit()
+print("Records created successfully")
 
 conn.close()
