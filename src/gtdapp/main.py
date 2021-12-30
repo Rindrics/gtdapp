c8780f94375bbacf4c20b20fb3e87b7492c44337
Akira Hayashi <smxshxishxad@me.com>
2021-12-30 15:32:31 +0900

    :muscle: Show blank page using Flask

--- src/gtdapp/main.py
+++ src/gtdapp/main.py
@@ -1,8 +1,15 @@
 import sys
 import tkinter
 import sqlite
+from sqlite import get_entries
+import sqlite3
+
+conn = sqlite3.connect("todo.db")
 
 root = tkinter.Tk()
 
 root.title("gtdapp")
+
+entries = get_entries(conn)
+
 root.mainloop()
