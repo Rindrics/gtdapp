34b16d601fbb148057377ba8c3ff45c826e600d1
Akira Hayashi <smxshxishxad@me.com>
2021-12-31 10:49:09 +0900

    :innocent: Initialize db only when it does not exist

--- src/gtdapp/sqlite.py
+++ src/gtdapp/sqlite.py
@@ -1,27 +1,26 @@
 import sqlite3
+from os.path import exists
+
+
 
 conn = sqlite3.connect("todo.db")
 print("Opened database successfully")
 
+if not exists("todo.db"):
+    conn.execute("""CREATE TABLE ITEM
+    (ID INT PRIMARY KEY     NOT NULL,
+    TITLE           TEXT    NOT NULL,
+    DESCRIPTION     CHAR(200));""")
+    print("Table created successfully")
 
-conn.execute("DROP TABLE IF EXISTS ITEM;")
-
-conn.execute("""CREATE TABLE ITEM
-         (ID INT PRIMARY KEY     NOT NULL,
-         TITLE           TEXT    NOT NULL,
-         DESCRIPTION     CHAR(200));""")
-print("Table created successfully")
-
-
-conn.execute("INSERT INTO ITEM (ID,TITLE,DESCRIPTION) \
+    conn.execute("INSERT INTO ITEM (ID,TITLE,DESCRIPTION) \
       VALUES (1, 'hoge', 'hogehoge')");
-conn.execute("INSERT INTO ITEM (ID,TITLE,DESCRIPTION) \
+    conn.execute("INSERT INTO ITEM (ID,TITLE,DESCRIPTION) \
       VALUES (2, 'fuga', 'fugafuga')");
-conn.execute("INSERT INTO ITEM (ID,TITLE,DESCRIPTION) \
+    conn.execute("INSERT INTO ITEM (ID,TITLE,DESCRIPTION) \
       VALUES (3, 'piyo', 'piyopiyo')");
-
-conn.commit()
-print("Records created successfully")
+    conn.commit()
+    print("Records created successfully")
 
 conn.close()
 
