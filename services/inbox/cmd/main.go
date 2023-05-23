044b329205dcf3f162e9c9e813c6b20a6505caa2
Akira Hayashi <smxshxishxad@me.com>
2023-05-24 07:04:20 +0900

    :wrench: Serve health status on `:8880`

--- services/inbox/cmd/main.go
+++ services/inbox/cmd/main.go
@@ -5,9 +5,12 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"net/http"
 	"os"
+	"sync"
 
 	"github.com/Rindrics/gtdapp-spec/services/inbox/internal"
+	"github.com/Rindrics/gtdapp-spec/services/inbox/internal/health"
 	_ "github.com/lib/pq"
 	_ "github.com/mattn/go-sqlite3"
 	"google.golang.org/grpc"
@@ -15,30 +18,54 @@ import (
 )
 
 func main() {
-	grpcServer := grpc.NewServer()
+	var wg sync.WaitGroup
+	wg.Add(2) // We will run two servers concurrently
 
-	dbDriver := os.Getenv("DB_DRIVER")
-	dbSource := os.Getenv("DB_SOURCE")
-	db, err := sql.Open(dbDriver, dbSource)
-	if err != nil {
-		log.Fatalf("failed to open database: %v", err)
-	}
+	// service endpoint on gRPC
+	go func() {
+		defer wg.Done()
+		grpcServer := grpc.NewServer()
 
-	inboxService := &internal.InboxService{
-		DB: db,
-	}
-	internal.RegisterInboxServer(grpcServer, inboxService)
-	reflection.Register(grpcServer)
+		dbDriver := os.Getenv("DB_DRIVER")
+		dbSource := os.Getenv("DB_SOURCE")
+		db, err := sql.Open(dbDriver, dbSource)
+		if err != nil {
+			log.Fatalf("gRPC server: failed to open database: %v", err)
+		}
 
-	port := ":50051"
-	listenPort, err := net.Listen("tcp", port)
-	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
-	}
+		inboxService := &internal.InboxService{
+			DB: db,
+		}
+		internal.RegisterInboxServer(grpcServer, inboxService)
+		reflection.Register(grpcServer)
 
-	fmt.Printf("Server is starting on port %s\n", port)
-	err = grpcServer.Serve(listenPort)
-	if err != nil {
-		log.Fatalf("failed to serve: %v", err)
-	}
+		port := ":50051"
+		listenPort, err := net.Listen("tcp", port)
+		if err != nil {
+			log.Fatalf("gRPC server: failed to listen: %v", err)
+		}
+
+		fmt.Printf("Server is starting on port %s\n", port)
+		err = grpcServer.Serve(listenPort)
+		if err != nil {
+			log.Fatalf("gRPC server: failed to serve: %v", err)
+		}
+	}()
+
+	// health endpoint on HTTP
+	go func() {
+		defer wg.Done()
+
+		mux := http.NewServeMux()
+		mux.HandleFunc("/health", health.HealthCheckHandler)
+
+		port := ":8880"
+		fmt.Printf("Serving health status on port %s\n", port)
+		err := http.ListenAndServe(port, mux)
+		if err != nil {
+			log.Fatalf("HTTP server: failed to start HTTP server: %v", err)
+		}
+	}()
+
+	wg.Wait()
 }
